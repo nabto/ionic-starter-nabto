95ccaf9924aed4a02661d47d0bcc9a23f73458cc
Ulrik Gammelby <ug@nabto.com>
2016-11-17 10:32:24 +0000

    use cordova plugin beta repo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 npm install
 ionic platform add ios browser
-ionic plugin add cordova-plugin-nabto --searchpath=/Users/nabto/git/cordova-plugin-nabto --verbose
-ionic plugin add cordova-plugin-device
+#ionic plugin add cordova-plugin-nabto --searchpath=/Users/nabto/git/cordova-plugin-nabto --verbose
+ionic plugin add cordova-plugin-nabto@2.0-beta.2
 echo 'OTHER_LDFLAGS = -force_load $(BUILT_PRODUCTS_DIR)/libCordova.a -lstdc++' >> platforms/ios/cordova/build.xcconfig
 
