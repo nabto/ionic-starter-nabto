63a5d58f5fdc21e9f09baebbffda38a6b20d6e8a
fusionfoxy <thom2287@gmail.com>
2017-01-06 09:35:04 +0100

    OneSignal push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { ToastController } from 'ionic-angular';
 import { OverviewPage } from '../pages/overview/overview';
+import { OneSignal } from 'ionic-native';
+
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
@@ -17,6 +19,23 @@ export class NabtoIonicApp {
       .then(() => {
         StatusBar.styleDefault();
         Splashscreen.hide();
+
+        ////// ONE SIGNAL SETUP /////
+        OneSignal.startInit("58a88e8b-83f1-429d-8863-8d8180ae83ed", "242279958126");
+        OneSignal.inFocusDisplaying(OneSignal.OSInFocusDisplayOption.InAppAlert);
+        OneSignal.setSubscription(true);
+        OneSignal.handleNotificationReceived().subscribe(() => {
+          // do something when the notification is received.
+        });
+        OneSignal.handleNotificationOpened().subscribe(() => {
+          // do something when the notification is opened.
+        });
+        OneSignal.endInit();    
+        
+        OneSignal.getIds().then(data => {
+          // this gives you back the new userId and pushToken associated with the device. Helpful.
+        });       
+        
       });
   }
 
