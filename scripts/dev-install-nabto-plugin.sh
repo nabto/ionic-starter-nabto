cfc4670d9bd94c71b55ece084ed74a397c603865
Ulrik Gammelby <ug@nabto.com>
2017-01-25 13:24:14 +0000

    added android helper scripts

--- scripts/dev-install-nabto-plugin.sh
+++ scripts/dev-install-nabto-plugin.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+CDV_PLUGIN_TAG=@2.0.0-beta.13
+CDV_LOCAL_GIT=${DIR}/../../cordova-plugin-nabto
+
 cordova plugin remove cordova-plugin-nabto
-cordova plugin add cordova-plugin-nabto@2.0.0-beta.11 --searchpath ~/git/cordova-plugin-nabto
+cordova plugin add cordova-plugin-nabto${CDV_PLUGIN_TAG} --searchpath ${CDV_LOCAL_GIT}
 
 echo 'OTHER_LDFLAGS = -force_load $(BUILT_PRODUCTS_DIR)/libCordova.a -lstdc++' >> platforms/ios/cordova/build.xcconfig
 
