519b31bfceb05017ea0f39ec7fb135ff4c09871d
fusionfoxy <thom2287@gmail.com>
2017-02-24 16:05:15 +0100

    code comments for educational purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,16 @@ export class NabtoIonicApp {
         Splashscreen.hide();
 
         ////// ONE SIGNAL SETUP /////
+        // startInit takes the app-id retreived from https://onesignal.com/apps/
+        // and the google project id retreived from https://console.developers.google.com
         OneSignal.startInit("58a88e8b-83f1-429d-8863-8d8180ae83ed", "242279958126");
+        // sets up the PN alerts and subscriptions
         OneSignal.inFocusDisplaying(OneSignal.OSInFocusDisplayOption.InAppAlert);
         OneSignal.setSubscription(true);
+        /* function called when PN is received,
+         * In case we check if its a silent notification (ie. from uNabto)
+         * If so, we read the contents and create a non-silent notification for the user
+         */
         OneSignal.handleNotificationReceived().subscribe((JsonData) => {
           // do something when the notification is received.
           console.log("ONESIGNAL NOTIFICATION RECEIVED: " + JSON.stringify(JsonData));
@@ -36,11 +43,14 @@ export class NabtoIonicApp {
             });
           }
         });
+        // function called when the user opens a notification, unused here
         OneSignal.handleNotificationOpened().subscribe(() => {
           console.log("ONESIGNAL NOTIFICATION OPENED");
           // do something when the notification is opened.
         });
-        OneSignal.endInit();    
+        OneSignal.endInit();
+        // Get the Player ID for this device.
+        // This is the ID the uNabto device must use as Player_id when sending a PN
         OneSignal.getIds().then(data => {
           console.warn("OneSignal userId: " + data.userId);
           // this gives you back the new userId and pushToken associated with the device. Helpful.
