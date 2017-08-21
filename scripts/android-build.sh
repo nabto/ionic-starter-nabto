14e1fbfb626d195e62097ab711e08c3e79a87f47
Ulrik Gammelby <ug@nabto.com>
2017-08-21 08:29:15 +0100

    use latest cordova plugin

--- scripts/android-build.sh
+++ scripts/android-build.sh
@@ -5,6 +5,10 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
+if [ ! -d node_modules ]; then
+    npm install
+fi
+
 ionic cordova platform add android@latest
 ionic cordova prepare android
 ionic cordova build android
