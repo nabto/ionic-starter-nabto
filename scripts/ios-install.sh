d53751ebcaa61edd0118954d07b7af042b31bd6d
Ulrik Gammelby <ug@nabto.com>
2018-01-23 11:48:55 +0100

    use old cordova to get rid of random odd problems when adding ios platform

--- scripts/ios-install.sh
+++ scripts/ios-install.sh
@@ -7,5 +7,7 @@ if [ "$EUID" != "0" ]; then
     exit 1
 fi
 
-npm install cordova ionic ios-sim@latest -g
+# old cordova installed as versions 7+ is observed to have problems installing iOS platform
+# (problem similar to the one described here: https://github.com/mapsplugin/cordova-plugin-googlemaps/issues/1435)
+npm install cordova@6.5.0 ionic ios-sim@latest -g
 npm install -g ios-deploy --unsafe-perm=true
