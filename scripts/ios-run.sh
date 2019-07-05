92938e1dcdba4fd6301d221816dca764b1e47f1b
Ulrik Gammelby <ug@nabto.com>
2019-07-05 15:55:20 +0200

    bump ios platform version, add trouble shooting hint to run script

--- scripts/ios-run.sh
+++ scripts/ios-run.sh
@@ -5,4 +5,5 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-ionic cordova run ios --livereload -c -s --debug --device -- --buildFlag="-UseModernBuildSystem=0"
+# if you get an error about missing a development team, add the following: "-- --developmentTeam=<team id>"
+ionic cordova run ios --livereload -c -s --debug --device
