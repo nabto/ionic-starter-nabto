6d58bc8578a31f88209bc4233968363c6958bbe7
Ulrik Gammelby <ug@nabto.com>
2019-07-29 15:31:57 +0200

    work around broken emulation with ionic cli (minor) update

--- scripts/ios-emulate.sh
+++ scripts/ios-emulate.sh
@@ -6,4 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
 # see available simulators with cordova emulate ios --list, then invoke with --target "iPhone-7, 10.3"
-ionic cordova emulate ios -l -c --target iPhone-8 
+# if you get an error about missing a development team, add the following: "-- --developmentTeam=<team id>"
+ionic cordova emulate ios -l -c --target iPhone-8 --no-native-run
